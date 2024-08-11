6e99b7253b0a13633f268fdacb309cd0b8fc4f0a
Saket Shukla <148642140+saket-shukla-0909@users.noreply.github.com>
2024-08-11 23:14:58 +0530

    Second commit of backend

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,15 @@
 import express from "express";
 const app = express();
 import  dotenv  from "dotenv";
+import { connectDatabase } from "./config/dbConnect.js";
 dotenv.config({ path: "backend/config/config.env"});
+
+// Connecting to Database
+    connectDatabase();
+
+// Import all routes
+import productRoutes from './routes/products.js';
+app.use("/api/v1", productRoutes);
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Server has started at PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
